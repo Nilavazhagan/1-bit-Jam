e9ecddb38f6e9d2bba23a0b3b8a1727f7be7de7b
Nilavazhagan <nilavazhagan1696@gmail.com>
2021-04-03 12:16:15 +0530

    Basic Player Controller and NPCController implemented.
    NPC Controller patrols between a set of points.

---
+++ Assets/Scripts/NPCController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 5f;
+    [SerializeField]
+    Transform[] patrolPoints;
+
+    bool frozen = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(patrolPoints.Length > 0)
+        {
+            transform.position = patrolPoints[0].position;
+            index = 1;
+        }
+    }
+
+    Vector2 startPos, dest;
+    float t = 0f;
+    int index = 1;
+    // Update is called once per frame
+    void Update()
+    {
+        if(t == 0f)
+        {
+            Debug.Log("Index :: " + index);
+
+            startPos = transform.position;
+            dest = patrolPoints[index].position;
+        }
+
+        if(t >= 1)
+        {
+            transform.position = dest;
+            t = 0f;
+            index = ++index % patrolPoints.Length;
+        }
+        else
+        {
+            t += Time.deltaTime * speed;
+
+            Vector2 newPos = startPos;
+            newPos.x = Mathf.Lerp(startPos.x, dest.x, t);
+            newPos.y = Mathf.Lerp(startPos.y, dest.y, t);
+
+            transform.position = newPos;
+        }
+    }
+
+
+    private static List<NPCController> _instances;
+
+    private static void Freeze()
+    {
+
+    }
+
+    private static void UnFreeze()
+    {
+
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float speed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMovement = Input.GetAxis("Horizontal");
+        float yMovement = Input.GetAxis("Vertical");
+
+        Vector2 newPos = transform.position;
+        newPos.x += xMovement * Time.deltaTime * speed;
+        newPos.y += yMovement * Time.deltaTime * speed;
+
+        transform.position = newPos;
+    }
+}
